729690ac6b82c82c69fe718ebcb1ae239dcd3e45
kartik <kaxxtric001@gmail.com>
2026-02-02 10:00:31 +0530

    added zod auth schemas

---
+++ Backend/Schemas/AuthSchema.ts
@@ -0,0 +1,17 @@
+import {z} from "zod";
+
+export const signupSchema=z.object({
+    email:z.string().email(),
+    name:z.string().min(2).max(50).trim(),
+    password:z.string().min(6).max(100),
+    role: z.enum(["PATIENT", "DOCTOR", "ADMIN"])
+
+});
+
+export const signinSchema=z.object({
+    email:z.string().email(),
+    password:z.string().min(5).max(10),
+});
+
+export type SignUpInput=z.infer<typeof signupSchema>;
+export type SignInInput=z.infer<typeof signinSchema>;
