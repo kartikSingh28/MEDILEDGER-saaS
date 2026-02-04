e595ba6c442ad019d8857fe24164efe48dcc81e7
kartik <kaxxtric001@gmail.com>
2026-02-04 22:59:46 +0530

    made multer storage disk to memory

--- Backend/config/multer.ts
+++ Backend/config/multer.ts
@@ -1,24 +1,9 @@
 import multer from "multer";
-import path from "path";
-import fs from "fs";
-
-const uploadPath = path.join(__dirname, "../../uploads");
-
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: uploadPath,
-  filename: (_req, file, cb) => {
-    const unique = Date.now() + "-" + file.originalname;
-    cb(null, unique);
-  },
-});
 
 export const upload = multer({
-  storage,
+  storage: multer.memoryStorage(),
+
   limits: {
-    fileSize: 10 * 1024 * 1024, // 10MB
-  },
+    fileSize: 10 * 1024 * 1024
+  }
 });
